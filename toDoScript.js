884d8e9576807923728cf39aa5de2f002cc15e97
Senseinvader <d.ivantsov1983@gmail.com>
2018-09-09 22:39:09 +0200

    Add listener autosave notes at page unload, delete save note button code refactor, delete drag function

--- toDoScript.js
+++ toDoScript.js
@@ -1,82 +1,72 @@
-document.getElementById('on-button-container').addEventListener('click', function() {
-  createDom("","");
-});
+document.getElementById('on-button-container').addEventListener('click', () => createDom("","")
+);
+
+window.onload = () => loadNotes();
 
 
 // -------FUNCTION TO FIT INPUT TEXTAREA TO AMOUNT OF INPUTTED TEXT------
 function autosize(){
-  let el = this;
+  const el = this;
   setTimeout(function(){
     el.style.cssText = 'height:auto; padding-top:5';
     el.style.cssText = 'height:' + el.scrollHeight + 'px';
   },0);
 }
 
-// --------FUNCTION TO REMOVE NOTE ELEMENT--------
-
-function closeNote(event) {
-  this.parentNode.parentNode.remove();
-}
-
 // ---------FUNCTION TO CREATE NOTE ELEMENT---------
 
-function createDom(noteName, noteContents) {
+function createDom(noteName, noteContent) {
   let allNotesContainer = document.getElementById('all-notes-container');
   let note = document.createElement('div');
   note.setAttribute('class', 'container');
 
   note.innerHTML = `<div class="container-header">
-                  <input class="note-header" type="text" placeholder="Name your note" value="${noteName}">
-                  <div class="close-note"> X </div>
-                </div>
-                <div class="container-body">
-                  <textarea class="note-body" type="text" placeholder="Your note is here:">${noteContents}</textarea>
-                </div>`;
+                      <input class="note-header" type="text" placeholder="Name your note" value="${noteName}">
+                    <div class="close-note"> &times; </div>
+                    </div>
+                    <div class="container-body">
+                      <textarea rows="9" class="note-body" type="text" placeholder="Your note is here:">${noteContent}</textarea>
+                    </div>`;
   
   allNotesContainer.appendChild(note);
 
-  let noteClosers = document.getElementsByClassName("close-note");
+  const noteClosers = document.getElementsByClassName("close-note");
   for(let i = 0; i < noteClosers.length; i++) {
     noteClosers[i].addEventListener('click', closeNote);
   }
-  let noteAreas = document.getElementsByTagName('textarea');
+  const noteAreas = document.getElementsByTagName('textarea');
   for(let i = 0; i < noteAreas.length; i++) {
     noteAreas[i].addEventListener('keydown', autosize);
+
+    this.addEventListener('beforeunload', saveNotes);
   }
-  // note.getElementsByTagName('textarea').addEventListener('keydown', autosize);
+
+  // const closers = Array.from(document.getElementsByClassName('close-note'));
+  // closers.forEach(element => {
+  //   addEventListener('click', closeNote);
+  // });
+
 }
 
-// --------FUCNTION TO DRAG NOTE onmousedown-onmouseup-------
-// let note = document.querySelector('.container');
-// note.onmousedown = function(event) {
+function saveNotes() {
+  localStorage.clear();
+  const noteNames = document.getElementsByClassName('note-header');
+  const noteContents = document.getElementsByClassName('note-body');
 
-//   let shiftX = event.clientX - note.getBoundingClientRect().left;
-//   let shiftY = event.clientY - note.getBoundingClientRect().top;
+  for (let i=0; i<noteNames.length; i++) {
+    localStorage.setItem(noteNames[i].value, noteContents[i].value);
+  }
+}
 
-//   moveAt(event.pageX, event.pageY);
+function loadNotes() {
+  for (let i=0; i<localStorage.length; i++) {
+    if (localStorage.key(i) === "" && localStorage.getItem(localStorage.key(i)) === "") continue;
+    createDom(localStorage.key(i), localStorage.getItem(localStorage.key(i)));
+  }
+}
 
-//   // centers the note at (pageX, pageY) coordinates
-//   function moveAt(pageX, pageY) {
-//     note.style.left = pageX - shiftX + 'px';
-//     note.style.top = pageY - shiftY + 'px';
-//   }
-
-//   function onMouseMove(event) {
-//     moveAt(event.pageX, event.pageY);
-//   }
-
-//   // move the note on mousemove
-//   document.addEventListener('mousemove', onMouseMove);
-
-//   // leave note, remove unneeded handlers
-//   note.onmouseup = function() {
-//     document.removeEventListener('mousemove', onMouseMove);
-//     note.onmouseup = null;
-//   };
-
-// };
-
-// note.ondragstart = function() {
-//   return false;
-// };
-   
+function closeNote() {
+  const header = this.parentNode.getElementsByTagName('input')[0].innerHTML;
+  localStorage.removeItem(header);
+  this.parentNode.parentNode.remove();
+}
